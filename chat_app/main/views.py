526753f5fd77bafa07135c4d0ff435f786d67878
Abhishek Hesh <heshabhishek21@gmail.com>
2021-10-05 22:38:00 +0530

    added request.method == POST

--- chat_app/main/views.py
+++ chat_app/main/views.py
@@ -5,13 +5,16 @@ def login(request):
     return render(request,'login.html')
 
 def home(request):
-    e_id = request.POST['Email_ID']
-    password = request.POST['Password']
-    data = {
-        'email_id' : e_id,
-        'password' : password,
-    }
-    if request.method == 'POST' and e_id and password:
-        return render(request,'home.html',data)
+    if request.method == 'POST':
+        e_id = request.POST['Email_ID']
+        password = request.POST['Password']
+        data = {
+            'email_id' : e_id,
+            'password' : password,
+        }
+        if e_id and password:
+            return render(request,'home.html',data)
+        else:
+            return render(request,'login.html')
     else:
         return render(request,'login.html')
