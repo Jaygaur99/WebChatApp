1244f781d3948b19b2b7357d96f3b8d218ddb75d
Abhishek Hesh <heshabhishek21@gmail.com>
2021-10-14 16:06:33 +0530

    fix conflicts

--- chat_app/main/models.py
+++ chat_app/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class signup(models.Model):
+    user_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    pass1 = models.models.CharField(max_length=100)
+    pass2 = models.models.CharField(max_length=100)
