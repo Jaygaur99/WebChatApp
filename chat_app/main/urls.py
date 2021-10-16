96e61b03a6600ce94d8867cc9c41bf4e6939bb49
Abhishek Hesh <heshabhishek21@gmail.com>
2021-10-16 15:25:51 +0530

    Worked on Templates

--- chat_app/main/urls.py
+++ chat_app/main/urls.py
@@ -5,17 +5,7 @@ from .views import *
 urlpatterns = [
     path('',welcome,name='welcome'), # Temporary
     path('login/', login, name='login'),
-<<<<<<< HEAD
-<<<<<<< HEAD
     path('signup/', signup, name='signup'),
     path('signhandle/', signhandle, name = 'signhandle'),
-    path('loginauth/', loginauth, name = 'loginauth')
-=======
-    path('', home, name='home')
->>>>>>> 9d8e99d6c4b553827d74154271812fbdc5feb2b5
-=======
-    path('signup/', signup, name='signup'),
-    path('signhandle/', signhandle, name = 'signhandle'),
-    path('loginauth/', loginauth, name = 'loginauth')
->>>>>>> b2b383746068e94a492b92ba658c0bf0c8c36f7b
+    path('loginauth/', loginauth, name = 'loginauth'),
 ]
