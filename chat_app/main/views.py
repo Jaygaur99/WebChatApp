1d9845a26e65a8025982026863cd3f5bc08697d3
Abhishek Hesh <heshabhishek21@gmail.com>
2021-10-17 11:30:48 +0530

    fix conflicts

--- chat_app/main/views.py
+++ chat_app/main/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import auth, messages 
 from django.contrib.auth.models import User 
 from .forms import *
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 # Create your views here.
 def login(request):
     return render(request,'main/login.html')
@@ -42,27 +45,15 @@ def signhandle(request):
         pass2 = request.POST['password_2']
         dob = request.POST['dob']
 
-        username = fname + lname
 
-        # check for errorneous input
-        if len(username)<10:
-            messages.error(request, " Your user name must be under 10 characters")
-            return redirect('signup')
-
-        if not username.isalnum():
-            messages.error(request, " User name should only contain letters and numbers")
-            return redirect('signup')
         if (pass1!= pass2):
              messages.error(request, " Passwords do not match")
              return redirect('signup')
         
         # Create the user
-        #myuser = User.objects.create_user(fname,lname, email, pass1,pass2,dob)
         myuser = User.objects.create_user(email=email, fname=fname, lname=lname, dob=dob, password=pass1)
-        myuser.first_name= fname
-        myuser.last_name= lname
         myuser.save()
         messages.success(request, " account has been successfully created")
         return redirect('login')
     else:
-        return HttpResponse("404 - Not found")
+        return redirect('welcome')
