06e50045f751003d0b71e21015b6408dd2903e8c
Jay Kumar Gaur <jaygaur99@gmail.com>
2021-10-18 16:34:36 +0530

    Migrated home.html to chat_room app and added app_name in urls.py

--- chat_app/main/views.py
+++ chat_app/main/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth import authenticate, login, get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User 
 from .forms import *
-from django.conf import settings
 from .models import *
 from .helpers import *
 
@@ -20,17 +19,17 @@ def loginauth(request):
         user = authenticate(request, username=e_id, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect('chat_room:home')
         else:
             messages.info(request,'Invalid Credentials')
-            return redirect('login')
+            return redirect('main:login')
     else:
-        return redirect('login')
+        return redirect('main:login')
 
-def home(request):
-    user = get_user_model()
-    all_users = user.objects.all()
-    return render(request,'main/home.html', {'allusers': all_users })
+# def home(request):
+#     user = get_user_model()
+#     all_users = user.objects.all()
+#     return render(request,'main/home.html', {'allusers': all_users })
 
 def signup(request):
     form = RegisterForm
@@ -51,15 +50,15 @@ def signhandle(request):
 
         if (pass1!= pass2):
              messages.error(request, " Passwords do not match")
-             return redirect('signup')
+             return redirect('main:signup')
         myuser = User.objects.create_user(email=email, fname=fname, lname=lname, dob=dob, password=pass1)
         myuser.first_name = fname
         myuser.last_name = lname
         myuser.save()
         messages.success(request, " account has been successfully created")
-        return redirect('login')
+        return redirect('main:login')
     else:
-        return redirect('signup')
+        return redirect('main:signup')
 
 def change_password(request):
     return render(request, 'main/change_password.html')
@@ -88,7 +87,7 @@ def verify_otp(request):
             error_message = 'Wrong Otp! Please Try Again.'
             return render(request, 'main/otp.html', {'email': email, 'error': error_message})
     else:
-        return redirect('login')
+        return redirect('main:login')
 
 def new_password(request):
     if request.method == 'POST':
@@ -103,7 +102,7 @@ def new_password(request):
         else:
             return render(request, 'main/new_password.html', {'email': email})
     else:
-        return redirect('login')
+        return redirect('main:login')
 
 # ----- Creating Views for friend request -------
 @login_required
