54aa2b2d74c82442773de000d1ab7ee0a145941a
Jay Kumar Gaur <jaygaur99@gmail.com>
2021-10-20 23:26:31 +0530

    Added functionality to directly chat from same page with javascript and event template loading

--- chat_app/chat_app/settings.py
+++ chat_app/chat_app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -153,3 +153,6 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "51110102961@piemr.edu.in"
 EMAIL_HOST_PASSWORD = "Bhavesh@8817"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- chat_app/chat_room/views.py
+++ chat_app/chat_room/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from .helpers import two_username_to_one_username
@@ -8,13 +9,24 @@ def home(request):
     all_users = user.objects.all()
     return render(request,'chat_room/home.html', {'allusers' : all_users })
 
+# def chat_person(request):
+#     User = get_user_model()
+#     email = request.POST.get('email')
+#     name = request.POST.get('name')
+#     user = User.objects.get(email=email)
+#     usernames = two_username_to_one_username(email, request.user.email)
+#     print(usernames)
+#     return render(request, 'chat_room/chat.html', {'user':user, 'name':name, 'usernames':usernames})
+
 def chat_person(request):
     User = get_user_model()
-    email = request.POST.get('email')
-    name = request.POST.get('name')
-    user = User.objects.get(email=email)
-    usernames = two_username_to_one_username(email, request.user.email)
-    print(usernames)
+    email = request.user.email
+    # print(json.loads(request.body))
+    name = json.loads(request.body).get('id')
+    # print(name)
+    user = User.objects.get(id=name)
+    usernames = two_username_to_one_username(email, user.email)
+    # print(usernames)
     return render(request, 'chat_room/chat.html', {'user':user, 'name':name, 'usernames':usernames})
 
 def index(request):
